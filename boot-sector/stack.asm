168945ad8c96ec421f4aef38ccdc4c3e38c707da
Percy Maldonado Quispe <percy.maldonado@ucsp.edu.pe>
2018-05-24 07:07:52 -0500

    stack, real adress init

--- boot-sector/stack.asm
+++ boot-sector/stack.asm
@@ -3,19 +3,25 @@
 	mov bp, 0x8000		; this is an address far away from 0x7c00 so that we don't get overwritten
 	mov sp, bp			; if the stack is empty then sp points to bp
 
+	mov al, [0x7ffe] ; 0x8000 - 2
+	int 0x10
+
 	push 'A'
 	push 'B'
 	push 'C'
 	push 'D'
 	push 'E'
 	push 'F'
-
+	
 	mov cx, 6
-	top:
-		pop bx
-		mov al, bl
-		int 0x10 			; prints
-	loop top
+	mov al, [0x7ffa]
+	int 0x10
+
+	;top:
+	;	pop bx
+	;	mov al, bl
+	;	int 0x10 			; prints		
+	;loop top
 
 	; data that has been pop'd from the stack is garbage now
 	mov al, [0x8000]
